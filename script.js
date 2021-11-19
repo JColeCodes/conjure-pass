5f5a073defc71f0e77140104e9c2545fb7f9a843
Jennifer Cole <capauldi@gmail.com>
2021-11-19 02:55:00 -0800

    added length prompt and character type confirmation

--- script.js
+++ script.js
@@ -11,13 +11,39 @@ var generatePassword = function() {
   var numbers = "0123456789";
   var chars = "!@#$%^&*()?<>.,':;-+=_";
 
+  // Ask user for password specifications
+  // Ask Length
+  var passLength = window.prompt("How many characters long do you want your password to be? (8-128, or * for random length)");
+  if (passLength == "*") {
+    passLength = randomNum(8,128);
+  } else {
+    passLength = parseInt(passLength);
+  }
+  if (!passLength || passLength < 8 || passLength > 128){
+    window.alert("Not a valid number length.");
+    return generatePassword();
+  }
+
+  // Ask if lowercase
+  var passLower = window.confirm("Would you like your password to have lowercase characters? (a, b, c...)");
+  console.log(passLower);
+  // Ask if UPPERCASE
+  var passUpper = window.confirm("Would you like your password to have UPPERCASE characters? (A, B, C...)");
+  console.log(passUpper);
+  // Ask if number
+  var passNum = window.confirm("Would you like your password to have numberical (number) characters? (1, 2, 3...)");
+  console.log(passNum);
+  // Ask if special characters
+  var passSpec = window.confirm("Would you like your password to have special characters? (!, $, &...)");
+  console.log(passSpec);
+
   // Assign requirements for the password
   var passwordRequirements = {
-    length: 8,
-    lowercase: true,
-    uppercase: true,
-    numeric: true,
-    specialchar: true
+    length: passLength,
+    lowercase: passLower,
+    uppercase: passUpper,
+    numeric: passNum,
+    specialchar: passSpec
   }
 
   // Create bank of possible characters for password
