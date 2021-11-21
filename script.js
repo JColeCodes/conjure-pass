977bc45dc9548a2a4a2557dcf38b8c20a719693d
Jennifer Cole <capauldi@gmail.com>
2021-11-20 20:36:46 -0800

    password requires each selected character type and password regeneration button added

--- script.js
+++ script.js
@@ -4,6 +4,16 @@
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var chars = "!@#$%^&*()?<>.,':;-+=_";
+var regen = false;
+
+// Default password requirements for global reach
+var passwordRequirements = {
+  len: null,
+  lowercase: null,
+  uppercase: null,
+  numeric: null,
+  specialchar: null
+}
 
 // Random!
 var randomNum = function(min, max) {
@@ -15,11 +25,13 @@ var passRequire = function() {
   // Ask user for password specifications
   // Ask Length
   var passLength = window.prompt("How many characters long do you want your password to be? (8-128, or * for random length)");
+  // If *, return random length
   if (passLength == "*") {
     passLength = randomNum(8,128);
   } else {
     passLength = parseInt(passLength);
   }
+  // If passLength is empty / not a number, less than 8, or more than 128, rerun function
   if (!passLength || passLength < 8 || passLength > 128){
     window.alert("Not a valid number length.");
     return passRequire();
@@ -40,7 +52,7 @@ var passRequire = function() {
   }
 
   // Assign requirements for the password
-  var passwordRequirements = {
+  passwordRequirements = {
     len: passLength,
     lowercase: passLower,
     uppercase: passUpper,
@@ -54,28 +66,72 @@ var passRequire = function() {
 // Generate Password
 var generatePassword = function() {
 
-  var passReq = JSON.parse(passRequire());
-
+  // If generated password has new requirements
+  if (!regen){
+    var passReq = JSON.parse(passRequire()); // If new, run passRequire
+  }
+  // Or regenerate password from already-entered requirements
+  else {
+    var passReq = passwordRequirements;
+  }
+  
   // Create bank of possible characters for password
+  var myPassword = "";
   var possChar = "";
+  var oneOfEach = []; // oneOfEach ensures at least one character from each selected type will make it into the password
+
+  // If lowercase letters are required
   if (passReq.lowercase) {
     possChar += alphabet;
+    // Randomly generate 1 lowercase letter that will be in the password
+    var alphaLow = alphabet.split("");
+    oneOfEach.push(alphaLow[randomNum(0,alphaLow.length)]);
   }
+  // If uppercase letters are required
   if (passReq.uppercase) {
     possChar += alphabet.toUpperCase();
+    // Randomly generate 1 uppercase letter that will be in the password
+    var alphaUpp = alphabet.toUpperCase().split("");
+    oneOfEach.push(alphaUpp[randomNum(0,alphaUpp.length)]);
   }
+  // If numbers are required
   if (passReq.numeric) {
     possChar += numbers;
+    // Randomly generate 1 number that will be in the password
+    var charNum = numbers.split("");
+    oneOfEach.push(charNum[randomNum(0,charNum.length)]);
   }
+  // If special characters are required
   if (passReq.specialchar) {
     possChar += chars;
+    // Randomly generate 1 special character that will be in the password
+    var charSpec = chars.split("");
+    oneOfEach.push(charSpec[randomNum(0,charSpec.length)]);
   }
+  // Bank of letters for the remaining letters of the password - Repeats very possible
   var letterBank = possChar.split("");
 
   // Generate random password
-  var myPassword = "";
   for (i = 0; i < passReq.len; i++) {
-    myPassword += letterBank[randomNum(0,letterBank.length)];
+    
+    if ((passReq.len - myPassword.length) <= oneOfEach.length) {
+      // Make sure all characters in one of each ARE in the password
+      var headTail = 0.4;
+    } else {
+      // Randomly pick if password pulls from letter bank or one of each array
+      var headTail = Math.random();
+    }
+
+    if (headTail < 0.5 && oneOfEach.length > 0) {
+      // If one of each, pull a random character from the array
+      var selectedChar = randomNum(0,oneOfEach.length);
+      myPassword += oneOfEach[selectedChar];
+      // Remove from array when used
+      oneOfEach.splice(selectedChar, 1);
+    } else {
+      // If else, pull random character from letter bank
+      myPassword += letterBank[randomNum(0,letterBank.length)];
+    }
   }
 
   return myPassword;
@@ -83,6 +139,21 @@ var generatePassword = function() {
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var regenBtn = document.querySelector("#regen");
+
+// Regenerate button is disabled by default
+regenBtn.disabled = true;
+
+function generate() {
+  regen = false;
+  writePassword();
+  // Regenerate button becomes active once requirements are initially set
+  regenBtn.disabled = false;
+}
+function regenerate() {
+  regen = true;
+  writePassword();
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -94,4 +165,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generate);
+regenBtn.addEventListener("click", regenerate);
